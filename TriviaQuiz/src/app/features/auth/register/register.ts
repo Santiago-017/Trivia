4d0ffbb8aea6fc091435d1366c360367ff7582b3
Santiago-017 <santiagobejaranoariza@gmail.com>
2025-11-18 19:40:56 -0500

    Se modifica el archivo register

--- TriviaQuiz/src/app/features/auth/register/register.ts
+++ TriviaQuiz/src/app/features/auth/register/register.ts
@@ -1,11 +1,15 @@
+// src/app/features/auth/register/register.ts
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],  // <--- IMPORTANTE
   templateUrl: './register.html',
-  styleUrl: './register.scss',
+  styleUrls: ['./register.scss'],
 })
-export class Register {
-
+export class Register{
+  // solo vista por ahora
 }
