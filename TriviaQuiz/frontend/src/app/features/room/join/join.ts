88340f76b54983540e233ae1810627380b662223
Santiago <santiagobejaranoariza@gmail.com>
2025-11-22 16:46:18 -0500

    Se agrega prototipo de componente join

--- TriviaQuiz/frontend/src/app/features/room/join/join.ts
+++ TriviaQuiz/frontend/src/app/features/room/join/join.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-join',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './join.html',
-  styleUrl: './join.scss',
+  styleUrls: ['./join.scss'],
 })
-export class Join {
-
-}
+export class JoinComponent {}
