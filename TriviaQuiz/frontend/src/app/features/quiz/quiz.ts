2441f254a52bb78587704e9162b63252bec52be0
Santiago-017 <santiagobejaranoariza@gmail.com>
2025-11-29 14:34:18 -0500

    Se crea la pagina para desarrollar el juego

--- TriviaQuiz/frontend/src/app/features/quiz/quiz.ts
+++ TriviaQuiz/frontend/src/app/features/quiz/quiz.ts
@@ -1,11 +1,23 @@
+// src/app/features/quiz/quiz.ts
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-quiz',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './quiz.html',
-  styleUrl: './quiz.scss',
+  styleUrls: ['./quiz.scss'],
 })
 export class Quiz {
+  question = 'WHAT IS THE LARGEST MAMMAL IN THE WORLD?';
 
+  options = ['ELEPHANT', 'LION', 'TIGER', 'BLUE WHALE'];
+
+  selectedIndex: number | null = null;
+
+  // Por ahora solo marcamos la respuesta seleccionada visualmente
+  selectOption(index: number): void {
+    this.selectedIndex = index;
+  }
 }
